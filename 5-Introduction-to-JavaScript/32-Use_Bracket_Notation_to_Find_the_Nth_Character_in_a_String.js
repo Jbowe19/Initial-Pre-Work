0c333c0fd9e8f821db695a61d2385d6bb1c18cd1
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 16:43:32 -0600

    found the third letter in the last name

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+/* found the third letter in the last name */
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
