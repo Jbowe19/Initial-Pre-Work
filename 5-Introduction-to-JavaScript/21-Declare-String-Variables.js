08d28e200d183ee247c0202ef268f5f4bc12e315
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 15:09:21 -0600

    declared a string

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+/* declared string variables */
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Jessica";
+var myLastName = "Bowe";
