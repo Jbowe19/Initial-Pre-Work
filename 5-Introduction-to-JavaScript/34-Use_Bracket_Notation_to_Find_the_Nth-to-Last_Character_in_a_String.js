80ae8658205fdc86e91c6e4b16a3ff68a2f26ff6
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 16:51:47 -0600

    found the second to last character in the last name

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+/* found the second to last character in the last name */
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
