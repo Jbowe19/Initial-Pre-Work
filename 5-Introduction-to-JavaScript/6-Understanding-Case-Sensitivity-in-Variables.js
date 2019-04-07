86623d0ea88fa5c0f24e41d98f8a79a1c8d4666d
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 13:54:10 -0600

    adjusted the camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+/* adjusted the camelCase */
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
