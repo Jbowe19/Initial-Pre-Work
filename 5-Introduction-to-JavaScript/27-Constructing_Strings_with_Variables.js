d05d7fd69994c3a21d4f49ddbc1cee48b32683b6
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 16:16:53 -0600

    used a variable to construct a string

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+/* used a variable to construct a string */
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Jessica Bowe";
+var myStr = "My name is " + myName + " and I am well!";
