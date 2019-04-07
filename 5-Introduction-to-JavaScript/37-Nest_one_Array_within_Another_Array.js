0aab0c2df2f4d54e180b912e7cc1a811c8a6bd6e
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 17:40:04 -0600

    created a nested array

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+/* created a nested array */
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["John", 36], ["Jane", 32]];
