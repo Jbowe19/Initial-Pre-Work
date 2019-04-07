11cca2f137d6559e4f4b2feba0c8462a823a25e9
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-07 17:26:22 -0600

    made a mad lip with word blanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+/* made a mad lip with word blanks */
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+   // Your code below this line
+  var result =  result+= "My very "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
