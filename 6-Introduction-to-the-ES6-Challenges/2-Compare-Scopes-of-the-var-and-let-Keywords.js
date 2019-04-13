e461071c02aa763570f5c1a868dfe324e0576b3e
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-13 15:32:13 -0600

    learned about scopes of var and let

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+/* learned about scopes of var and let */
+
+function checkScope() {
+ "use strict";
+let i = "function scope";
+  if (true) {
+  let i = "block scope";
+ console.log("Block scope i is: ", i);
+  }
+console.log("Function scope i is: ", i);
+return i;
+}
