16f7d5c87ceff6cfa0ac131d01a277366e21e3b7
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-13 15:36:46 -0600

    used const instead of var

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,9 @@
+/* used const instead of var */
+function printManyTimes(str) {
+  "use strict";
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+}
+printManyTimes("freeCodeCamp");
