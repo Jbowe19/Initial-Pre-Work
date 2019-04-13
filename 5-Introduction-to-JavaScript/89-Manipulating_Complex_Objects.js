3648e7834932c890b95ca6121f7f80595d090cbc
Jessica Bowe <Jessbowe18@gmail.com>
2019-04-13 13:41:19 -0600

    created a complex data structure

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+/* created a complex data structure */
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Johnny Cash",
+    "title": "Folsom Prison",
+    "release_year": 1968,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+  ],
+ }
+];
